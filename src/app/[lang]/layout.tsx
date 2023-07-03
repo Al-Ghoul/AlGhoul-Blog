9ce0edd0294465d4decb35ded41585d792fdfd38
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2023-07-03 16:01:03 +0200

    Add tailwind-scrollbar plugin

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -25,7 +25,9 @@ export default async function RootLayout({
   const LL = i18nObject(params.lang as Locales);
 
   return (
-    <html lang={params.lang} className={`bg-[url('/../images/background.png')] bg-cover bg-no-repeat ${inter.className} ${CairoFont.variable} ${Aref_Ruqaa.variable} bg-center md:bg-unset bg-fixed`}>
+    <html lang={params.lang} className={`bg-[url('/../images/background.png')] bg-cover bg-no-repeat ${inter.className} ${CairoFont.variable} ${Aref_Ruqaa.variable} bg-center md:bg-unset bg-fixed
+    scrollbar-thin scrollbar-thumb-[#326C85] scrollbar-track-[#4A3470] scrollbar-thumb-rounded-lg
+    `}>
       <body>
 
         <Header lang={params.lang} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,6 +23,7 @@ module.exports = {
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar')({ nocompatible: true }),
   ],
 }
