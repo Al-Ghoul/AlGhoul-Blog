b75af2470bcc190afbc6faef54dc0a9532c77d49
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-01-28 11:09:44 +0200

    Remove pre-commit fron inputs (devenv provides it automatically)

--- flake.nix
+++ flake.nix
@@ -4,10 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     devenv.url = "github:cachix/devenv/9ba9e3b908a12ddc6c43f88c52f2bf3c1d1e82c1";
-    pre-commit-hooks = {
-      url = "github:cachix/pre-commit-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   nixConfig = {
